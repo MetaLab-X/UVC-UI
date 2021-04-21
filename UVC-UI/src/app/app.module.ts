cf309c00e67feeec25429084d742034a4d5f4448
Ratchadaaa <ratchaporn_pat@vu.ac.th>
2021-04-22 00:12:14 +0700

    final

--- UVC-UI/src/app/app.module.ts
+++ UVC-UI/src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { from } from 'rxjs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MatSlideToggleModule,
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
